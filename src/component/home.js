92704cd094ee676af0d436c18775677ed420b825
Humayoun shah <humayoun.sha31@gmail.com>
2022-01-26 19:51:45 +0500

    footer

--- src/component/home.js
+++ src/component/home.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import '../main.css'
+import instagram from '../images/insta.png'
+import twitter from '../images/twitter.png'
+import discord from '../images/Discord-Logo.png'
 // import Minting from './minting';
 class Home extends React.Component {
   state = {};
@@ -7,7 +11,16 @@ class Home extends React.Component {
       <div className='home-container'>
         <div className='header'>Header</div>
         <div className='content'>Content</div>
-        <div className='footer'>Footer</div>
+        <div className='footer'>
+          <div className='footer-left'>
+
+          </div>
+          <div className='footer-right'>
+            <a><img src={instagram}/></a>
+            <a><img src={twitter}/></a>
+            <a><img src={discord}/></a>
+          </div>
+        </div>
       </div>
     );
   }
