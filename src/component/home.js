433e1328756c40c1bbd48cb3f1c8ca66c5094827
fhyem <fhyembercha@gmail.com>
2022-01-26 17:57:35 +0500

    adsf

--- src/component/home.js
+++ src/component/home.js
@@ -3,7 +3,7 @@ import React from 'react';
 class Home extends React.Component {
   state = {};
   render() {
-    return <div>Hello from Home component</div>;
+    return <div>Hello from the Home side!~~~~~~</div>;
   }
 }
 
